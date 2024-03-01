e3fd4f19c11d66e163e26a99eb06072faed04a03
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-01 15:30:41 +0530

    updated

--- Controller-vs-RestController/src/main/java/lk/ijse/spring/WebAppConfig.java
+++ Controller-vs-RestController/src/main/java/lk/ijse/spring/WebAppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan
 @EnableWebMvc
 public class WebAppConfig {
 }
