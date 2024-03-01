77b8f586228e31630dbc7ce73fb713c02cfefedc
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-01 15:34:30 +0530

    controller updated

--- Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController1.java
+++ Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController1.java
@@ -1,2 +1,10 @@
-package lk.ijse.spring.api;public class MyController1 {
+package lk.ijse.spring.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController1 {
+    public MyController1() {
+        System.out.println("My Controller1 - constructor");
+    }
 }
