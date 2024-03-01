73bc68aa488df79b99f99a35dc5c1259c3e27792
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-01 10:56:22 +0530

    bean updated

--- hello-spring-webmvc/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ hello-spring-webmvc/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBean {
     public SpringBean() {
         System.out.println("SpringBean-constructor");
