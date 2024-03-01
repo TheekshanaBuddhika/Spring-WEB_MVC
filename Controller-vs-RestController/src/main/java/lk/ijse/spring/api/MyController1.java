ed361aa00ddf0894e6c334530187f240a62045e7
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-01 15:59:09 +0530

    Controllers Mapped

--- Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController1.java
+++ Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController1.java
@@ -1,10 +1,19 @@
 package lk.ijse.spring.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/one")
 public class MyController1 {
     public MyController1() {
         System.out.println("My Controller1 - constructor");
     }
+
+    @GetMapping
+    public String getMethod(){
+        return "Mycontroller1";
+    }
+
 }
--- Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController2.java
+++ Controller-vs-RestController/src/main/java/lk/ijse/spring/api/MyController2.java
@@ -1,10 +1,19 @@
 package lk.ijse.spring.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/two")
 public class MyController2 {
     public MyController2() {
         System.out.println("MyController2 - constructor");
     }
+
+    @GetMapping
+    public String getMethod(){
+        return "Mycontroller2";
+    }
+
 }
