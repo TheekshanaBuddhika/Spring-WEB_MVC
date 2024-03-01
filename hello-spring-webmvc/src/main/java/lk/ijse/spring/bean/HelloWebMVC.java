d6b4b33b4850e99f15514ae5a29d525181e78131
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-01 12:01:59 +0530

    bean created

---
+++ hello-spring-webmvc/src/main/java/lk/ijse/spring/bean/HelloWebMVC.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.bean;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWebMVC {
+}
--- hello-spring-webmvc/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ hello-spring-webmvc/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -1,10 +1,45 @@
 package lk.ijse.spring.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, DisposableBean, InitializingBean {
     public SpringBean() {
         System.out.println("SpringBean-constructor");
     }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("SpringBean-bean name awre");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("SpringBean-bean factory name awre");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("SpringBean-application context awre");
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("SpringBean-Initializing bean");
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("SpringBean-destroy");
+
+    }
+
+
 }
