998f9f8dadcc67f36dc5e2f1b003a365fd193c7d
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-14 09:14:44 +0530

    order controller created

---
+++ Mapping-Specification/src/main/java/lk/ijse/api/OrderController.java
@@ -0,0 +1,18 @@
+package lk.ijse.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping
+    public String getOrders(){
+        return "getOrders";
+    }
+
+
+
+}
