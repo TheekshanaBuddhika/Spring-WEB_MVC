0e4d3906aee88b0b7ec66845ae55c528d90def46
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-03-25 08:42:03 +0530

    updated

--- Spring-web-project/src/main/java/lk/ijse/spring/api/CustomerController.java
+++ Spring-web-project/src/main/java/lk/ijse/spring/api/CustomerController.java
@@ -1,5 +1,39 @@
 package lk.ijse.spring.api;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@CrossOrigin("*")
 public class CustomerController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<CustomerDTO> getcustomer(){
+
+        ArrayList<CustomerDTO>CustomerDTOs= new ArrayList<>();
+        CustomerDTOs.add(0,new CustomerDTO("C001","Kamal","Galle"));
+        CustomerDTOs.add(1,new CustomerDTO("C002","Kamal","Matara"));
+        CustomerDTOs.add(2,new CustomerDTO("C003","Kamal","Colombo"));
+
+        return  CustomerDTOs;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveCustomer(@RequestBody CustomerDTO customerDTO){
+
+        System.out.println(customerDTO.toString());
+        return;
+
+    }
+
+    @DeleteMapping(value = "{/id}")
+    public void deleteCustomer(@PathVariable("id") String id){
+        System.out.println(id);
+        return;
+    }
+
 }
