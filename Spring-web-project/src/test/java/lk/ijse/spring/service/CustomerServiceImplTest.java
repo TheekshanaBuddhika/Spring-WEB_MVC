917adf4b3c35cd85900b4f0fab33bf47e3f3e469
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-04-04 09:26:11 +0530

    test class added

---
+++ Spring-web-project/src/test/java/lk/ijse/spring/service/CustomerServiceImplTest.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceImplTest {
+
+    @Test
+    void getAllCustoemr() {
+    }
+
+    @Test
+    void getCustomerDetails() {
+    }
+
+    @Test
+    void saveCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}
